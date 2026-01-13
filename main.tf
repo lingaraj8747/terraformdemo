acb880e9682e96d7bff171d25b7eb372f51373d5
lingaraj8747 <lingarajmaravalli6@gmail.com>
2026-01-13 12:08:32 +0530

    add main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "aws" {
+    region = "  "
+    ami = " "
+    instance_type = " "
+    vpc_security_group_ids = [" "] 
+    tags = {
+
+       Name = "my-ce2-instance"
+   }
+
+    
+}
+
